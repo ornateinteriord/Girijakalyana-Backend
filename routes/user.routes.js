f874bc5ae1ae91b2162da7e0f5ecc40b0d39cf5d
Suresh <suris6530@gmail.com>
2025-04-11 11:38:34 +0530

    user_profile

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getProfileByRegistrationNo, updateProfile } = require('../controllers/profileController');
+const { getProfileByRegistrationNo, updateProfile, getAllUserDetails } = require('../controllers/profileController');
 const authenticateToken = require('../middleware/auth.middleware');
 
 
@@ -8,6 +8,6 @@ const authenticateToken = require('../middleware/auth.middleware');
 
 router.get('/profile/:registration_no',authenticateToken,getProfileByRegistrationNo);
 router.put('/update-profile/:registration_no',authenticateToken,updateProfile);
-
+router.get('/all-users-profiles',authenticateToken,getAllUserDetails);
 
 module.exports = router;
