6c76e6bd96f222ced713b10aedb5b3bc445a9f06
spexzee <abub5979@gmail.com>
2025-07-27 06:24:02 +0530

    Implement pagination for received and sent interests; update routes to use POST method for interest retrieval

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,13 +11,13 @@ router.post('/all-users-profiles', authenticateToken, getAllUserDetails);
 
 // Interest routes (with authentication and consistent naming)
 router.post("/interest", authenticateToken, expressInterest);
-router.get("/interest/sent/:sender",authenticateToken,  getSentInterests);
+router.post("/interest/sent/:sender",authenticateToken,  getSentInterests);
 router.delete('/cancel',authenticateToken, cancelInterestRequest);
 
 router.get("/interest/status/:sender/:recipient", authenticateToken, getInterestStatus);
 router.put("/interest/:registration_no", authenticateToken, updateInterestStatus);
-router.get("/interest/received/:recipient", authenticateToken, getReceivedInterests);
-router.get("/interest/accepted/:recipient", authenticateToken, getAcceptedInterests);
+router.post("/interest/received/:recipient", authenticateToken, getReceivedInterests);
+router.post("/interest/accepted/:recipient", authenticateToken, getAcceptedInterests);
 router.post("/change-password/:registration_no",authenticateToken, changePassword);
 router.get("/interest-counts/:registrationNo", getInterestCounts);
 router.get("/search",authenticateToken, searchUsersByInput);
